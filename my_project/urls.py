2e4a29dec7b6109a9421896ffce3b8b65deb6861
14sammie41 <q85e7u2t@students.codeinstitute.net>
2025-08-14 18:52:04 +0100

    Made the second app work and made an else if statement that worked in views.py

--- my_project/urls.py
+++ my_project/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from hello_world import views as index_views
+from about import views as about_views
 
 urlpatterns = [
-    path('',index_views.hello_world, name='index'),
+    path('about/',about_views.about_views, name='about'),
+    path('hello/',index_views.hello_world, name='index'),
     path('admin/', admin.site.urls),
 ]
